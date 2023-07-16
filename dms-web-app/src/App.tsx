5bd12a0474708f6633606d249c3530b747c2c47b
Melih DUMANLI <melihdumanli@hotmail.com>
2023-07-17 00:01:45 +0300

    Useless items and methods removed

--- dms-web-app/src/App.tsx
+++ dms-web-app/src/App.tsx
@@ -1,30 +1,14 @@
-import React, { useState, useEffect } from 'react';
-import { createRoot } from "react-dom/client";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './pages/Home';
 import Login from './pages/Login';
-import Profile from './pages/Profile';
-
-const root = createRoot(document.getElementById("root")!);
 
 const App: React.FC = () => {
-    const [token, setToken] = useState<string | null>(localStorage.getItem('token'));
-
-    useEffect(() => {
-        console.log(token);
-    }, []);
-
-    const handleLogout = () => {
-        setToken(null);
-        localStorage.removeItem('token');
-    };
-
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Login />} />
                 <Route path="/login" element={<Login />} />
-                <Route path="/profile" element={<Profile />} />
                 <Route path="/home" element={<Home />} />
             </Routes>
         </BrowserRouter>
