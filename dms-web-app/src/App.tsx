7ca5995b6ccbb8e661d9de3d008e32197c583e0b
Melih DUMANLI <melihdumanli@hotmail.com>
2023-07-16 21:52:07 +0300

    Login/Register page completed.

--- dms-web-app/src/App.tsx
+++ dms-web-app/src/App.tsx
@@ -1,12 +1,15 @@
 import React, { useState } from 'react';
-import { BrowserRouter, Routes, Route, Link, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Home from './pages/Home';
 import Login from './pages/Login';
-import Register from './pages/Register';
 import Profile from './pages/Profile';
 
 const App: React.FC = () => {
     const [token, setToken] = useState<string | null>(localStorage.getItem('token'));
+
+    const handleLogin = (token: string) => {
+        console.log(token,)
+    }
 
     const handleLogout = () => {
         // Clear the token on logout
@@ -18,46 +21,14 @@ const App: React.FC = () => {
 
     return (
         <BrowserRouter>
-        <div>
-            <nav>
-                <ul>
-                    <li>
-                        <Link to="/">Home</Link>
-                    </li>
-                    {!isAuthenticated && (
-                        <>
-                            <li>
-                                <Link to="/login">Login</Link>
-                            </li>
-                            <li>
-                                <Link to="/register">Register</Link>
-                            </li>
-                        </>
-                    )}
-                    {isAuthenticated && (
-                        <>
-                            <li>
-                                <Link to="/profile">Profile</Link>
-                            </li>
-                            <li>
-                                <button onClick={handleLogout}>Logout</button>
-                            </li>
-                        </>
-                    )}
-                </ul>
-            </nav>
-
-                <Routes>
-                    <Route path="/" element={<Login />}>
-                        {!isAuthenticated && <Route path="/login" element={<Login />} />}
-                        {!isAuthenticated && <Route path="/register" element={<Register />} />}
-                        {isAuthenticated && <Route path="/profile" element={<Profile />} />}
-                        {isAuthenticated && <Route path="/home" element={<Home />} />}
-                        {!isAuthenticated && <Route path="/*" element={<Navigate to="/login" replace />} />}
-                    </Route>
-                </Routes>
-
-        </div>
+            <Routes>
+                <Route path="/" element={<Login handleLogin={handleLogin} />}>
+                    {!isAuthenticated && <Route path="/login" element={<Login handleLogin={handleLogin} />} />}
+                    {isAuthenticated && <Route path="/profile" element={<Profile />} />}
+                    {isAuthenticated && <Route path="/home" element={<Home />} />}
+                    {!isAuthenticated && <Route path="/*" element={<Navigate to="/login" replace />} />}
+                </Route>
+            </Routes>
         </BrowserRouter>
     );
 };
--- dms-web-app/src/pages/Login.tsx
+++ dms-web-app/src/pages/Login.tsx
@@ -1,29 +1,59 @@
 import React, { useState } from 'react';
-import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
-import '../styles/login.scss';
+import axios from 'axios';
+import { ToastContainer, toast } from 'react-toastify';
+import '../assets/styles/login.scss';
+import "react-toastify/dist/ReactToastify.css";
 
-const Login: React.FC = () => {
+interface LoginProps {
+    handleLogin: (token: string) => void;
+}
+const loginUrl = 'http://localhost:8080/api/v1/auth/login';
+const registerUrl = 'http://localhost:8080/api/v1/auth/register';
+
+const Login: React.FC<LoginProps> = ({ handleLogin }) => {
+    const [firstname, setFirstName] = useState('')
+    const [lastname, setLastName] = useState('')
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [isLoginForm, setIsLoginForm] = useState(true)
     const [error, setError] = useState('');
     const navigate = useNavigate();
 
-    const handleLogin = async (e: React.FormEvent) => {
+    const notify = () => {
+        toast.error(error, {
+            position: toast.POSITION.TOP_RIGHT
+        });
+    };
+
+    const handleSubmit = async (e: React.FormEvent) => {
         e.preventDefault();
-
-        try {
-            const response = await axios.post('API_LOGIN_ENDPOINT', {
+        isLoginForm
+            ? axios.post(loginUrl, {
                 email,
-                password,
-            });
-
-            const { token } = response.data;
-            localStorage.setItem('token', token);
-            navigate('/home');
-        } catch (error) {
-            console.error('Login failed:', error);
-        }
+                password
+            })
+                .then(response => {
+                    localStorage.setItem('access_token', response.data.access_token);
+                    localStorage.setItem('refresh_token', response.data.refresh_token);
+                })
+                .catch(error => {
+                    setError("Wrong email or password. Please try again.")
+                    notify()
+                })
+            : axios.post(registerUrl, {
+                firstname,
+                lastname,
+                email,
+                password
+            })
+                .then(response => {
+                    setIsLoginForm(true)
+                })
+                .catch(error => {
+                    setError("Register request failed. Please try again.")
+                    notify()
+                })
     };
     const validateEmail = () => {
         const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
@@ -33,27 +63,85 @@ const Login: React.FC = () => {
     };
 
     return (
-        <div className="login-container">
-            <h1>Giriş Yap</h1>
-            <form className="login-form" onSubmit={handleLogin}>
-                <div className="row">
-                    <input
-                        type="text"
-                        placeholder="E-Posta"
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                        onBlur={() => validateEmail()}
-                    />
-                    <img src="../assets/images/icon-tick.png" alt="valid" width="32" height="32" />
+        <div className="container">
+            <div className="login-container">
+                <div className="header-container">
+                    <div
+                        className="header"
+                        onClick={() => setIsLoginForm(true)}
+                        style={{
+                            backgroundColor: isLoginForm ? 'transparent' : '#fff',
+                            borderTopRightRadius: 0,
+                            color: isLoginForm ? '#fff' : '#333'
+                        }}>Login</div>
+                    <div
+                        className="header"
+                        onClick={() => setIsLoginForm(false)}
+                        style={{
+                            backgroundColor: isLoginForm ? '#fff' : 'transparent',
+                            borderTopLeftRadius: 0,
+                            color: isLoginForm ? '#333' : '#fff'
+                        }}>Register</div>
                 </div>
-                <input
-                    type="password"
-                    placeholder="Şifre"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                />
-                <button type="submit">Login</button>
-            </form>
+                <form className="login-form" onSubmit={handleSubmit}>
+                    {isLoginForm && (
+                        <div>
+                            <div className="row">
+                                <input
+                                    type="text"
+                                    placeholder="E-Mail"
+                                    value={email}
+                                    onChange={(e) => setEmail(e.target.value)}
+                                    onBlur={() => validateEmail()}
+                                />
+                            </div>
+                            <input
+                                type="password"
+                                placeholder="Password"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                            />
+                        </div>
+                    )}
+                    {!isLoginForm && (
+                        <div>
+                            <div className="row">
+                                <input
+                                    type="text"
+                                    placeholder="First Name"
+                                    value={firstname}
+                                    onChange={(e) => setFirstName(e.target.value)}
+                                />
+                            </div>
+                            <div className="row">
+                                <input
+                                    type="text"
+                                    placeholder="Lastname"
+                                    value={lastname}
+                                    onChange={(e) => setLastName(e.target.value)}
+                                />
+                            </div>
+                            <div className="row">
+                                <input
+                                    type="text"
+                                    placeholder="E-Mail"
+                                    value={email}
+                                    onChange={(e) => setEmail(e.target.value)}
+                                    onBlur={() => validateEmail()}
+                                />
+                            </div>
+                            <input
+                                type="password"
+                                placeholder="Password"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                            />
+                        </div>
+                    )}
+                    <button type="submit">Submit</button>
+                </form>
+                <ToastContainer />
+            </div>
         </div>
     );
 };
